c7c28b2af81f1efcd120759d225482056ef2a8be
Mathias Munene <munenemathias@gmail.com>
2025-01-28 08:12:44 +0300

    get the user input

--- sripts/script.js
+++ sripts/script.js
@@ -2,3 +2,6 @@
 document.getElementById("akanForm").addEventListener("submit", function(event) {
     event.preventDefault(); 
     
+// Get user input
+const birthday = new Date(document.getElementById("birthday").value);
+const gender = document.getElementById("gender").value;   
